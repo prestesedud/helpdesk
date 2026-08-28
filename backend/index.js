754576f899c62d641b6aecf2f74b9303f414b597
David Prestes <davidupts@gmail.com>
2026-08-28 17:39:28 -0300

    feat: adiciona rota de login com JWT e corrige protecao de variaveis de ambiente sensíveis

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const db = require("./database");
 const chamadosRouter = require("./routes/chamados");
+const authRouter = require("./routes/auth");
 
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Servidor rodando!");
 });
-
 app.use("/chamados", chamadosRouter);
-
+app.use("/auth", authRouter);
 app.listen(3000, () => {
   console.log("O servidor está rodando na porta 3000!");
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,22 +2,59 @@ const express = require("express");
 const db = require("../database");
 const argon2 = require("argon2");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 router.post("/cadastro", async (req, res) => {
   const { nome, email, senha } = req.body;
   const hash = await argon2.hash(senha);
-  const usuarios = db.prepare(
-    `
+
+  try {
+    const usuarios = db.prepare(
+      `
     INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)
     `,
+    );
+    const resultado = usuarios.run(nome, email, hash);
+    res.status(201).json({
+      id: resultado.lastInsertRowid,
+      nome,
+      email,
+      role: "usuario",
+    });
+  } catch (erro) {
+    console.error(erro);
+    res.status(409).json({
+      erro: "Dados incorretos!",
+      campo: "email",
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, senha } = req.body;
+  const usuario = db
+    .prepare(
+      `
+    SELECT * FROM usuarios WHERE email = ?`,
+    )
+    .get(email);
+  if (!usuario) {
+    res.status(401).json({ message: "Credenciais incorretas!" });
+    return;
+  }
+  const verificacaoSenha = await argon2.verify(usuario.senha, senha);
+  if (!verificacaoSenha) {
+    res.status(401).json({ message: "Credenciais Inválidas" });
+    return;
+  }
+  const token = jwt.sign(
+    { id: usuario.id, role: usuario.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    },
   );
-  const resultado = usuarios.run(nome, email, hash);
-  res.status(201).json({
-    id: resultado.lastInsertRowid,
-    nome,
-    email,
-    role: "usuario",
-  });
+  res.status(200).json({ token });
 });
 
 module.exports = router;
