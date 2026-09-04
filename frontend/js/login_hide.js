6228fd1fb64962c430ba5ad88378a664ae8d59ca
David Prestes <davidupts@gmail.com>
2026-09-03 22:47:57 -0300

    feat: inserção de dados do login do frontend para o backend

--- frontend/js/login_hide.js
+++ frontend/js/login_hide.js
@@ -14,3 +14,50 @@ btnLogin.addEventListener("click", (e) => {
   cadastroForm.classList.add("hide");
   loginForm.classList.remove("hide");
 });
+
+const formularioLogin = document.querySelector(".login-form");
+
+formularioLogin.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  console.log("Formulário enviado!");
+
+  const emailLogin = document.getElementById("email_login").value.trim();
+  const senhaLogin = document.getElementById("passwd_login").value;
+
+  if (!emailLogin || !senhaLogin) {
+    alert("Por favor, preencha todos os campos!");
+    return;
+  }
+
+  if (senhaLogin.length < 6) {
+    alert("A senha deve ter pelo menos 6 caracteres.");
+    return;
+  }
+
+  try {
+    const resposta = await fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: emailLogin,
+        senha: senhaLogin,
+      }),
+    });
+
+    const dados = await resposta.json();
+    if (resposta.ok) {
+      console.log("Login bem sucedido: ", dados);
+    } else {
+      alert(`Erro no login: ${dados.mensagem || "credenciais inválidas"}`);
+    }
+  } catch (erro) {
+    console.error("Erro na requisição: ", erro);
+    alert("Erro na conexão. Tente novamente mais tarde.");
+  }
+
+  formularioLogin.reset();
+
+  //cadastro
+});
