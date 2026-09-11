2e0cf4a7fca3c8c1eaf1b1b147efe5293368349b
prestesedud <davidupts@gmail.com>
2026-09-10 23:26:09 -0300

    feat: implementar listagem de chamados do backend

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,6 +3,36 @@ const validaToken = localStorage.getItem("token");
 if (!validaToken) {
   window.location.href = "login.html";
 }
+
+async function buscarChamados() {
+  const token = localStorage.getItem("token");
+  const resposta = await fetch("http://localhost:3000/chamados", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dados = await resposta.json();
+  console.log(dados);
+
+  const arrayChamados = dados.map((chamado) => {
+    const cardHtml = `<li class="chamado-item">
+  <div class="card-header">
+    <span class="num-ticket">#${chamado.id}</span>
+    <span class="prior-ticket">${chamado.prioridade}</span>
+  </div>
+  <div class="chamado-conteudo">
+    <h3>${chamado.titulo}</h3>
+  </div>
+  <div class="chamado-footer"></div>
+</li>`;
+    return cardHtml;
+  });
+  const chamados = document.querySelector(".chamados");
+  chamados.innerHTML = arrayChamados.join("");
+}
+
+buscarChamados();
+
 //menu toggle
 const profileIconBtn = document.querySelector("#profile-icon-btn");
 const dropdownList = document.querySelector("#dropdown-list");
